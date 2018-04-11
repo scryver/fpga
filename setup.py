302d7f1674b5c1da4d7838862f73b5d2c2fc37f1
scryver <scryver@users.noreply.github.com>
2018-04-11 13:38:56 +0200

    Bump version of myhdl

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(name='fpga',
       version='0.1.1',
       description='FPGA modules for conversion to VHDL',
       author='Michiel',
-      install_requires=['myhdl>=1.0.dev0'],
+      install_requires=['myhdl>=0.10'],
       packages=['fpga', 'fpga.basics', 'fpga.encoders', 'fpga.generators', 'fpga.interfaces', 'fpga.tests'])
